914d8d5817b15405bc08016fd12cf14382e472bd
Park Jung-Hwan <wjdghks5698@gmail.com>
2023-01-19 20:31:52 +0900

    [추가] MemberForm 생성, memberList.html, createMemberForm.html 생성 [수정] 회원관리 예제 MemberController 회원등록, 회원목록 Method 추가

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,14 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller //해당 어노테이션을 통해 스프링이 뜰 때 해당 컨트롤러를 객체로 가지고 있다 == 스프링 빈이 관리된다.
 public class MemberController {
@@ -17,5 +23,31 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService){
         this.memberService = memberService;
+    }
+
+    //members 폴더의 createMemberForm.html 파일을 찾아서 화면에 내려줌.
+    //GET : 조회시에만 사용.
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    //POST : 데이터를 form에 넣어서 전달시에 사용
+    @PostMapping("/members/new")
+    public String create(MemberForm form){ //파라미터로 넘어온 MemberForm의 field로 화면의 input의 name의 값이 setName()을 통해 입력됨.
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members",members);
+
+        return "members/memberList";
     }
 }
